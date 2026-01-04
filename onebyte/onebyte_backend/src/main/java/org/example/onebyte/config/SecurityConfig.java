1126d35a40d3e1288ce78ca61808a6080ce6e917
jeongbeomgyu <jeongbeomgyu@github.com>
2026-01-04 22:28:09 +0900

    [feat] 접근 허용 URL 추가

--- onebyte/onebyte_backend/src/main/java/org/example/onebyte/config/SecurityConfig.java
+++ onebyte/onebyte_backend/src/main/java/org/example/onebyte/config/SecurityConfig.java
@@ -41,7 +41,8 @@ public class SecurityConfig {
                 "http://localhost:3000",
                 "http://127.0.0.1:3000",
                 "http://localhost:5173",
-                "http://127.0.0.1:5173"
+                "http://127.0.0.1:5173",
+                "http://44.220.167.111:3000"
         ));
 
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
